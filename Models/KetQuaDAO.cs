b09bc4a6b5f18124f60ed23175325ee04350e541
duy10 <duy10@DESKTOP-THD7QSI>
2025-03-06 22:28:47 +0700

    ketqua

---
+++ Models/KetQuaDAO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebQLDaoTao.Models
+{
+    public class KetQuaDAO
+    {
+        //--------doc danh sach cac kết quả theo mã môn học trong CSDL-----------------
+        public List<KetQua> getByMaMH(string mamh)
+        {
+            List<KetQua> ds = new List<KetQua>();
+            //1.Mo ket noi CSDL
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["WebQLDaoTao_Constr1"].ConnectionString);
+            conn.Open();
+            //2.tao truy van
+            SqlCommand cmd = new SqlCommand("select * from ketqua where mamh=@mamh", conn);
+            cmd.Parameters.AddWithValue("@mamh", mamh);
+            //3.thuc thi ket qua;
+            SqlDataReader rd = cmd.ExecuteReader();
+            //4.xu ly ket qua tra ve
+            while (rd.Read())
+            {
+                //tao doi tuong ketqua
+                KetQua kq = new KetQua()
+                {
+                    Id = int.Parse(rd["id"].ToString()), // id chuyển thành Int
+                    MaSV = rd["MaSV"].ToString(), // masv chuyển thành string
+                    MaMH = rd["mamh"].ToString() // mamh chuyển thành string
+
+                };
+                if (!string.IsNullOrEmpty(rd["diem"].ToString())) // nếu có điểm sẽ là false , null là true;
+                {
+                    kq.Diem = float.Parse(rd["diem"].ToString());
+                }
+                ds.Add(kq);
+            }
+            return ds;
+        }
+    }
+}
--- QLSinhvien.aspx.cs
+++ QLSinhvien.aspx.cs
@@ -5,16 +5,57 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebQLDaoTao.Models;
+
 
 namespace WebQLDaoTao
 {
     public partial class QLSinhvien : System.Web.UI.Page
     {
+        SinhVienDAO svDao = new SinhVienDAO();
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            if (!Page.IsPostBack)
+            {
+
+
+            }
         }
 
-       
+        protected void btShowForm_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        protected void btThem_Click(object sender, EventArgs e)
+        {
+            string masv = txtMaSV.Text;
+            string hosv = txtHoSv.Text;
+            string tensv = txtTenSV.Text;
+            bool gioitinh = rdNam.Checked ? true : false;
+            DateTime ngaysinh = DateTime.Parse(txtNgaysinh.Text);
+            string noisinh = txtNoiSinh.Text;
+            string diachi = txtDiaChi.Text;
+            string makh = ddlMaKhoa.SelectedValue;
+
+            SinhVien sv = new SinhVien()
+            {
+                MaSV = masv,
+                HoSV = hosv,
+                TenSV = tensv,
+                GioiTinh = gioitinh,
+                NgaySinh = ngaysinh,
+                NoiSinh = noisinh,
+                DiaChi = diachi,
+                MaKH = makh,
+            };
+            svDao.Insert(sv);
+            gvSinhVien.DataBind();
+        }
+
     }
-    }
+
+
+}
