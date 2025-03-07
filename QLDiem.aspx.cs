7c3dcbcda3dfbcec310dc2940bc1e41e513e3adc
ADMIN <ADMIN@PC218>
2025-03-07 09:20:53 +0700

    fix lan thu N

--- QLDiem.aspx.cs
+++ QLDiem.aspx.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using WebQLDaoTao.Models;
 namespace WebQLDaoTao
 {
     public partial class QLDiem : System.Web.UI.Page
     {
+        KetQuaDAO kqDAO = new KetQuaDAO();
         protected void Page_Load(object sender, EventArgs e)
         {
 
